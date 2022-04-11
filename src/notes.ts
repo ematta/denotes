998bf18ef31a738586b8ced53cf31187ab008008
Enrique Matta-Rodriguez <enrique.j.matta@gmail.com>
2022-04-11 18:47:36 -0500

    feat: search with test

--- src/notes.ts
+++ src/notes.ts
@@ -14,3 +14,10 @@ export async function write(args: Args): Promise<boolean> {
 export async function read() {
   return await Deno.readTextFile(config.filePath);
 }
+
+export async function search(term: string) {
+  const notes = await read();
+  const lines = notes.split('\n');
+  const results = lines.filter((line) => line.includes(term));
+  return results;
+}
