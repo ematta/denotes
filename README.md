7d1264fb07f86a42a4b91386cf71dcd8526c78d7
Enrique Matta-Rodriguez <220164+ematta@users.noreply.github.com>
2022-04-11 18:50:54 -0500

    Feature: Simple Search (#10)

---
+++ README.md
@@ -0,0 +1,9 @@
+```
+Usage:
+  denotes [options] <message>
+
+Options:
+  -h           Print usage
+  -r           Read notes
+  -s <term>    Search notes
+```
--- tests/notes_test.ts
+++ tests/notes_test.ts
@@ -1,5 +1,5 @@
 import { asserts, mocks } from '../deps.ts';
-import { read, write } from '../src/notes.ts';
+import { read, search, write } from '../src/notes.ts';
 
 Deno.test('test read', async () => {
   const mockedReadFile = mocks.stub(Deno, 'readTextFile', () => 'foo');
@@ -14,4 +14,22 @@ Deno.test('test write', async () => {
   const spy = mocks.spy(Deno, 'writeTextFile');
   await write({ _: ['foo'] });
   mocks.assertSpyCalls(spy, 1);
+});
+
+Deno.test("search should return an empty array if there's no match", async () => {
+  const stub = mocks.stub(Deno, 'readTextFile', () => "bar");
+  try {
+    asserts.assertEquals(await search("foo"), []);
+  } finally {
+    stub.restore();
+  }
+});
+
+Deno.test("search should return an array of lines that contain the term", async () => {
+  const stub = mocks.stub(Deno, 'readTextFile', () => "bar");
+  try {
+    asserts.assertEquals(await search("bar"), ['bar']);
+  } finally {
+    stub.restore();
+  }
 });
